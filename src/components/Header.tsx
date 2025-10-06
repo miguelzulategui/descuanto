cfd237a5b6abbb53d8843bd26ec02b87cc7eb8f7
miguelzulategui <jmzulategui@hotmail.com>
2025-10-06 19:12:06 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="header">
       <Link href="/" className="brand">
-        <Image src="/logo.png" alt="Descuanto" width={148} height={100} priority />
+        <Image src="/logo.png" alt="Descuanto" width={148} height={125} priority />
       </Link>
       <nav className="topnav">
         <Link href="/pricing">Para marcas</Link>
