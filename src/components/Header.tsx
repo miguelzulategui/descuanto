0cdc0e786e0310b98fc7065d902fba0500f906ef
miguelzulategui <jmzulategui@hotmail.com>
2025-10-06 19:18:13 +0200

    Update Header.tsx
    Cambios header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,15 @@ export default function Header() {
   return (
     <header className="header">
       <Link href="/" className="brand">
-        <Image src="/logo.png" alt="Descuanto" width={148} height={100} priority />
+        <Image
+          src="/logo.png"
+          alt="Descuanto"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ height: 40, width: "auto" }}
+          priority
+        />
       </Link>
       <nav className="topnav">
         <Link href="/pricing">Para marcas</Link>
