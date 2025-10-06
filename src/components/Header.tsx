feea3f32886352ed328f75f2cad3a886cf2ff54d
miguelzulategui <jmzulategui@hotmail.com>
2025-10-06 18:37:29 +0200

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <Link href="/" className="brand">
+        <Image src="/logo.png" alt="Descuanto" width={148} height={40} priority />
+      </Link>
+      <nav className="topnav">
+        <Link href="/pricing">Para marcas</Link>
+      </nav>
+    </header>
+  );
+}
